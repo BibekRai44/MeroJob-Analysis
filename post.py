eec368156ac5476d723748e1de02a0ace0dfca31
Bibekkun <bibekrai655@gmail.com>
2023-03-07 17:24:22 +0545

    url changed

--- post.py
+++ post.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 urls = ['https://merojob.com/category/it-telecommunication/?page={}'.format(page) for page in range(1, 11)]
 
@@ -12,5 +13,5 @@ with open('hrefs.txt', 'w') as file:
         parent_links = soup.find_all('h1', class_='text-primary font-weight-bold media-heading h4')
         for parent_link in parent_links:
             children = parent_link.find('a')
-            href_value = children.get('href')
+            href_value = "https://merojob.com"+children.get('href')
             file.write(href_value + '\n')
