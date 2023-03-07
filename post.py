5571aff5bc03321ae9368753704ef32626a9312b
Bibekkun <bibekrai655@gmail.com>
2023-03-07 09:11:40 +0545

    url extraction

---
+++ post.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+urls = ['https://merojob.com/category/it-telecommunication/?page={}'.format(page) for page in range(1, 11)]
+
+with open('hrefs.txt', 'w') as file:
+
+    for url in urls:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        parent_links = soup.find_all('h1', class_='text-primary font-weight-bold media-heading h4')
+        for parent_link in parent_links:
+            children = parent_link.find('a')
+            href_value = children.get('href')
+            file.write(href_value + '\n')
